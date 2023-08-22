31f2be1989e3b2f3d467e4878b391d858bc62db9
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-22 15:52:49 -0300

    prebuild + teste BottomSheet App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { OptionButton } from './src/components/OptionButton';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <OptionButton
+          source={require('./assets/iconToDo.png')}
+          onPress={() => console.log('a')}
+        />
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={0}
+          snapPoints={['2%', '35%']}
+        >
+          <OptionButton
+            source={require('./assets/iconCalendar.png')}
+            onPress={() => console.log('a')}
+          />
+        </BottomSheet>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
