3695195c08a6b1fae3a82b66a8339e44484722e6
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-23 11:19:48 -0300

    Configurando para usar ref

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -1,32 +1,36 @@
-import { ReactNode, useRef } from 'react';
+import React, { ReactNode, useRef } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Colors } from '../../helpers/Colors';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet from '@gorhom/bottom-sheet';
+import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 
 interface BottomSheetInterface {
     children: ReactNode
 }
 
-export function BottomSheetComponent({ children }: BottomSheetInterface) {
-    const bottomSheetRef = useRef<BottomSheet>(null);
-
-    return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <BottomSheet
-                ref={bottomSheetRef}
-                index={0}
-                snapPoints={[24, '35%']}
-                handleStyle={{ backgroundColor: Colors.secondary, borderTopRightRadius: 12, borderTopLeftRadius: 12 }}
-                handleIndicatorStyle={{ backgroundColor: Colors.white, width: '35%' }}
-            >
-                <View style={styles.bottomContainer}>
-                    {children}
-                </View>
-            </BottomSheet>
-        </GestureHandlerRootView>
-    )
-}
+export const BottomSheetComponent = React.forwardRef<BottomSheetMethods, BottomSheetInterface>(
+    (
+        { children }: BottomSheetInterface,
+        ref: React.ForwardedRef<BottomSheetMethods>
+    ): JSX.Element =>{
+        return (
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <BottomSheet
+                    ref={ref}
+                    index={0}
+                    snapPoints={[24, '35%']}
+                    handleStyle={{ backgroundColor: Colors.secondary, borderTopRightRadius: 12, borderTopLeftRadius: 12 }}
+                    handleIndicatorStyle={{ backgroundColor: Colors.white, width: '35%' }}
+                >
+                    <View style={styles.bottomContainer}>
+                        {children}
+                    </View>
+                </BottomSheet>
+            </GestureHandlerRootView>
+        )
+    }
+)
 const styles = StyleSheet.create({
     container: {
         flex: 1,
