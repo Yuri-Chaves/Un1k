56c2f12d4f46d2618591173fa9f87960ea4752a2
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-23 11:19:21 -0300

    Usando Navegação

--- App.tsx
+++ App.tsx
@@ -1,41 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { OptionButton } from './src/components/OptionButton';
-import BottomSheet from '@gorhom/bottom-sheet';
-import { useRef } from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
+import Routes from './src/routes';
+import { NavigationContainer } from "@react-navigation/native";
+import { Colors } from './src/helpers/Colors';
 
 export default function App() {
-  const bottomSheetRef = useRef<BottomSheet>(null);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <OptionButton
-          source={require('./assets/iconToDo.png')}
-          onPress={() => console.log('a')}
-        />
-        <BottomSheet
-          ref={bottomSheetRef}
-          index={0}
-          snapPoints={['2%', '35%']}
-        >
-          <OptionButton
-            source={require('./assets/iconCalendar.png')}
-            onPress={() => console.log('a')}
-          />
-        </BottomSheet>
-        <StatusBar style="auto" />
-      </View>
-    </GestureHandlerRootView>
+      <NavigationContainer>
+          <Routes />
+          <StatusBar backgroundColor={Colors.primary} style={'inverted'} translucent={false} />
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
