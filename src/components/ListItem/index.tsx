30fe1998a2efccf71a685da084704d0d582c442e
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-24 08:50:17 -0300

    Componente ListItem v2

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Modal, Pressable, View } from 'react-native';
 
 import { Colors } from '../../helpers/Colors'
 
@@ -6,38 +7,105 @@ import { Entypo } from '@expo/vector-icons';
 
 import {
     Container,
+    TaskContainer,
+    OptionContainer,
+    PriorityText,
     Dot,
-    Text
+    Text,
+    ModalContainer,
+    ButtonsContainer,
+    Button,
 } from './style'
+
 interface ItemProps {
     dot?: boolean
-    text: string
     textDecoration?: boolean
+    text: string
+    priority: "low" | "normal" | "high"
 }
 
-export function ListItem({ dot, text, textDecoration }: ItemProps) {
+export function ListItem({ dot, text, textDecoration, priority }: ItemProps) {
     const [checked, setChecked] = useState<boolean>(false)
+    const [open, setOpen] = useState<boolean>(false)
+    const [isPressed, setIsPressed] = useState({
+        edit: false,
+        remove: false
+    })
+    const txtColor = checked ? Colors.grey3 : priority == "low" ? Colors.successD : priority == "normal" ? Colors.warningD : Colors.errorD
 
     return (
-        <Container
-        onPress={() => setChecked(!checked)}
-        >
-            {dot && (
-            <Dot
-                style={{
-                    backgroundColor: checked? '#008639' : '#0000',
-                    borderColor: checked? '#008639' : Colors.primary
-                }}
+        <Container>
+            <TaskContainer
+                onPress={() => setChecked(!checked)}
+                style={{ borderColor: txtColor }}
             >
-                <Entypo name='check' color={checked? '#FFF' : '#0000'} />
-            </Dot>
-            )}
-            <Text
-                style={{
-                    color: checked? '#05AC4B' : Colors.primary,
-                    textDecorationLine: textDecoration ? checked? 'line-through' : 'none' : 'none'
-                }}
-            >{text}</Text>
+                <PriorityText style={{ color: txtColor }}>{priority == "low" ? "Baixa" : priority == "high" ? "Alta" : "Normal"}</PriorityText>
+                {dot && (
+                    <Dot
+                        style={{
+                            borderColor: txtColor
+                        }}
+                    >
+                        <Entypo name='check' color={checked ? txtColor : '#0000'} size={18} />
+                    </Dot>
+                )}
+                <Text
+                    style={{
+                        color: txtColor,
+                        textDecorationLine: textDecoration && checked ? 'line-through' : 'none'
+                    }}
+                >{text}</Text>
+            </TaskContainer>
+            <Modal
+                animationType='slide'
+                transparent
+                visible={open}
+                onRequestClose={() => setOpen(!open)}
+            >
+                <Pressable
+                    onPress={() => setOpen(false)}
+                    style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                >
+                    <ModalContainer
+                        style={{ borderColor: txtColor }}
+                    >
+                        <Text>{text}</Text>
+                        <Text>
+                            Prioridade: <Text style={{ color: txtColor }}>{priority == "low" ? "Baixa" : priority == "high" ? "Alta" : "Normal"}</Text>
+                        </Text>
+                        <ButtonsContainer>
+                            <Button
+                                style={{ borderColor: '#A36FF7', backgroundColor: isPressed.edit? "#A36FF7" : "#0000" }}
+                                onPressIn={() => setIsPressed({...isPressed, edit: true})}
+                                onPressOut={() => setIsPressed({...isPressed, edit: false})}
+                                onPress={() => console.log('edit')}
+                            >
+                                <>
+                                    <Entypo name='pencil' color={isPressed.edit? Colors.grey5 : "#A36FF7"} size={18} />
+                                    <Text style={{ color: isPressed.edit? Colors.grey5 : "#A36FF7" }}>Editar</Text>
+                                </>
+                            </Button>
+                            <Button
+                                style={{ borderColor: '#F25F5C', backgroundColor: isPressed.remove? "#F25F5C" : "#0000" }}
+                                onPressIn={() => setIsPressed({...isPressed, remove: true})}
+                                onPressOut={() => setIsPressed({...isPressed, remove: false})}
+                                onPress={() => console.log('remove')}
+                            >
+                                <>
+                                    <Entypo name='trash' color={isPressed.remove? Colors.grey5 : "#F25F5C"} size={18} />
+                                    <Text style={{ color: isPressed.remove? Colors.grey5 : "#F25F5C" }}>Remover</Text>
+                                </>
+                            </Button>
+                        </ButtonsContainer>
+                    </ModalContainer>
+                </Pressable>
+            </Modal>
+            <OptionContainer
+                onPress={() => setOpen(true)}
+                style={{ borderColor: txtColor }}
+            >
+                <Entypo name='dots-three-vertical' color={txtColor} size={18} />
+            </OptionContainer>
         </Container>
     )
 }
--- src/components/ListItem/style.ts
+++ src/components/ListItem/style.ts
@@ -1,23 +1,73 @@
 import styled from 'styled-components/native'
 import { Colors } from '../../helpers/Colors'
 
-export const Container = styled.Pressable`
+export const Container = styled.View`
+    flex-direction: row;
+    width: 100%;
+`
+export const TaskContainer = styled.Pressable`
     flex-direction: row;
     gap: 8px;
-    width: 100%;
-    padding-left: 16px;
-    height: 32px;
+    width: 92%;
+    padding: 16px 16px;
+    border-radius: 8px 0px 0px 8px;
+    align-items: flex-start;
+    border-width: 2px;
+    border-style: solid;
+    position: relative;
+    border-right-width: 0px;
+`
+export const OptionContainer = styled.Pressable`
+    flex-direction: row;
+    width: 8%;
+    border-radius: 0px 8px 8px 0px;
     align-items: center;
+    justify-content: center;
+    border-width: 2px;
+    border-style: solid;
+    border-left-width: 0px;
 `
 export const Dot = styled.View`
-    width: 16px;
-    height: 16px;
-    border: 1px solid ${Colors.primary};
-    border-radius: 8px;
+    width: 24px;
+    height: 24px;
+    border: 2px solid ${Colors.primary};
+    border-radius: 4px;
     align-items: center;
     justify-content: center;
 `
 export const Text = styled.Text`
-    font-size: 16px;
+    font-size: 18px;
     font-weight: 600;
+`
+export const PriorityText = styled.Text`
+    font-size: 10px;
+    position: absolute;
+    top: 0px;
+    left: 4px;
+`
+export const ModalContainer = styled.View`
+    max-width: 80%;
+    padding: 16px;
+    border-radius: 8px;
+    background-color: ${Colors.grey5};
+    border-width: 2px;
+    border-style: solid;
+`
+export const ButtonsContainer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 8px;
+`
+export const Button = styled.Pressable`
+    width: 45%;
+    border-radius: 4px;
+    padding: 2px 0px;
+    border-width: 2px;
+    border-style: solid;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
 `
