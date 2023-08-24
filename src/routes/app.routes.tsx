58c99733ce639e20238cc500f984a73894644af8
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-24 10:58:23 -0300

    Configurando navegação para página lista de compra

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../pages/Home";
 import ToDo from "../pages/ToDo";
+import Shopping from "../pages/Shopping";
 
 export type StackNavigatorPages = {
     Home: undefined
     ToDo: undefined
+    Shopping: undefined
 }
 
 const Stack = createStackNavigator<StackNavigatorPages>();
@@ -15,6 +17,7 @@ export function AppRoutes() {
         <Stack.Navigator>
             <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
             <Stack.Screen name="ToDo" component={ToDo} options={{ headerShown: false }} />
+            <Stack.Screen name="Shopping" component={Shopping} options={{ headerShown: false }} />
         </Stack.Navigator>
     )
 }
