f5752f1b2864fb484d47f9199d94f211673db7cc
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-24 10:53:46 -0300

    removendo GestureHandlerRootView

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Colors } from '../../helpers/Colors';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 
@@ -15,20 +14,18 @@ export const BottomSheetComponent = React.forwardRef<BottomSheetMethods, BottomS
         ref: React.ForwardedRef<BottomSheetMethods>
     ): JSX.Element => {
         return (
-            <GestureHandlerRootView style={{ flex: 1 }}>
-                <BottomSheet
-                    ref={ref}
-                    index={0}
-                    style={{width:'100%'}}
-                    snapPoints={[24, '35%']}
-                    handleStyle={{ backgroundColor: Colors.secondary, borderTopRightRadius: 12, borderTopLeftRadius: 12 }}
-                    handleIndicatorStyle={{ backgroundColor: Colors.white, width: '35%' }}
-                >
-                    <View style={styles.bottomContainer}>
-                        {children}
-                    </View>
-                </BottomSheet>
-            </GestureHandlerRootView>
+            <BottomSheet
+                ref={ref}
+                index={0}
+                style={{ width: '100%' }}
+                snapPoints={[24, '35%']}
+                handleStyle={{ backgroundColor: Colors.secondary, borderTopRightRadius: 12, borderTopLeftRadius: 12 }}
+                handleIndicatorStyle={{ backgroundColor: Colors.white, width: '35%' }}
+            >
+                <View style={styles.bottomContainer}>
+                    {children}
+                </View>
+            </BottomSheet>
         )
     }
 )
