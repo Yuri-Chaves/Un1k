8c0d10d0d746484a5b1cd36e54f431b3cbb57cc5
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-24 10:59:22 -0300

    Criando página lista de compras

---
+++ src/pages/Shopping/index.tsx
@@ -0,0 +1,83 @@
+import { useRef, useState } from 'react';
+import { Dimensions } from 'react-native'
+
+import { Colors } from '../../helpers/Colors'
+import { BottomSheetComponent } from '../../components/BottomSheet'
+import { ListItem } from '../../components/ListItem'
+
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigatorPages } from "../../routes/app.routes";
+import { StackNavigationProp } from '@react-navigation/stack'
+import { AddItemForm } from '../../components/forms/AddItem';
+
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+
+import {
+    Container,
+    TitleContainer,
+    ActionsContainer,
+    ActionButton,
+    Text,
+    FlatListContainer,
+    Separator
+} from './style'
+
+import BottomSheet from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet';
+import { FlatList } from 'react-native-gesture-handler';
+
+export default function Shopping() {
+    const windowHeight = Dimensions.get('window').height;
+    const listHeight = windowHeight - 204
+    const navigation = useNavigation<StackNavigationProp<StackNavigatorPages>>()
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
+    return (
+        <Container>
+            <TitleContainer>
+                <Text
+                    style={{ fontWeight: "700", marginBottom: 8 }}
+                    color={Colors.primary}
+                    fontSize='48px'
+                >
+                    Compras
+                </Text>
+            </TitleContainer>
+            <ActionsContainer>
+                <ActionButton
+                    style={{ backgroundColor: Colors.errorM }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Text><Ionicons name='return-up-back' size={18} /></Text>
+                    <Text>Voltar</Text>
+                </ActionButton>
+                <ActionButton
+                    style={{ backgroundColor: Colors.successM }}
+                    onPress={() => bottomSheetRef.current?.expand()}
+                >
+                    <Text><MaterialIcons name='add-shopping-cart' size={18} /></Text>
+                    <Text>Nova</Text>
+                </ActionButton>
+            </ActionsContainer>
+            <FlatListContainer
+                style={{ height: listHeight }}
+            >
+                <FlatList
+                    data={[]}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <ListItem text={item} priority={item} textDecoration dot />
+                    )}
+                    ItemSeparatorComponent={() => <Separator />}
+                />
+            </FlatListContainer>
+            <BottomSheetComponent
+                ref={bottomSheetRef}
+            >
+                <AddItemForm
+                    onConfirm={() => bottomSheetRef.current?.collapse()}
+                    placeholder="Não esqueça de conferir a prioridade..."
+                />
+            </BottomSheetComponent>
+        </Container >
+    )
+}
