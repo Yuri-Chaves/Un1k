465d78fbce85536ee40d696d8f836a382f954143
Yuri-Chaves <Yuri.Chaves39@hotmail.com>
2023-08-25 10:07:39 -0300

    Adicionando contexto

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import 'react-native-gesture-handler';
 import Routes from './src/routes';
 import { NavigationContainer } from "@react-navigation/native";
 import { Colors } from './src/helpers/Colors';
+import { DBContextProvider } from './src/contexts/DBContext';
 
 export default function App() {
 
   return (
-      <NavigationContainer>
-          <Routes />
-          <StatusBar backgroundColor={Colors.secondary} style={'inverted'} translucent={false} />
-      </NavigationContainer>
+    <NavigationContainer>
+      <DBContextProvider>
+        <Routes />
+        <StatusBar backgroundColor={Colors.secondary} style={'inverted'} translucent={false} />
+      </DBContextProvider>
+    </NavigationContainer>
   );
 }
